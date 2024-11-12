6f0591425a17fba513e2d49dca2648c27ed8dbf5
Fabricio Crespo Rossel <crf0028382@est.univalle.edu>
2024-11-11 21:00:16 -0400

    Subir pdf terminad
    Terminado sin validaciones

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,44 @@
-
-/* src/App.js */
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min'; 
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import Home from './pages/Home';
-import About from './pages/About';
-import Login from './pages/Login'; 
-import Register from './pages/Register'; 
+import ListaPersonalSalud from './pages/ListaPersonalSalud';
+import RegistrarEstablecimiento from './pages/RegistrarEstablecimiento';
+import RegistrarPersonalSalud from './pages/RegistrarPersonalSalud';
+import ActualizarPersonalSalud from './pages/ActualizarPersonalSalud';
+import Tratamiento from './pages/tratamiento';
 import Paciente from './pages/Paciente';
-import Medico from './pages/Medico';
+import AñadirPaciente from './pages/AñadirPaciente';
+import ActualizarPaciente from './pages/ActualizarPaciente';
 import Transferencia from './pages/Transferencia';
+
+
+
+
+
+
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/pacientes' element={<Paciente />} />
-        <Route path='/medicos' element={<Medico />} />
-        <Route path='/transferencia' element={<Transferencia />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lista-personal-salud" element={<ListaPersonalSalud />} />
+          <Route path="/registrar-establecimiento" element={<RegistrarEstablecimiento />} />
+          <Route path="/registrar-personal-salud" element={<RegistrarPersonalSalud />} />
+          <Route path="/actualizar-personal-salud" element={<ActualizarPersonalSalud />} />
+          <Route path="/seguimiento-tratamientos" element={<Tratamiento />} />
+          <Route path="/lista-pacientes" element={<Paciente />} />
+          <Route path="/añadir-paciente" element={<AñadirPaciente />} /> 
+          <Route path="/actualizar-paciente/:id" element={<ActualizarPaciente />} />
+          <Route path="/transferencia" element={<Transferencia />} />
+
+        </Routes>
+      </Layout>
     </Router>
   );
 }
